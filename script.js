1d897e7ddbba0b992126ea8bfca49911cff83dc3
jj_leo <jrsnleons@gmail.com>
2022-01-07 15:19:16 +0800

    pomodoro app implementation

--- script.js
+++ script.js
@@ -76,3 +76,147 @@ deleteAllBtn.onclick = ()=>{
 
 
 //-----------------------TODO APP END----------------------------------------------------
+
+//---------------------POMODORO APP START --------------------------------------------
+
+//timer----------
+const el = document.querySelector(".clock");
+const bell = document.querySelector("audio");
+
+const mindiv = document.querySelector(".mins");
+const secdiv = document.querySelector(".secs");
+
+const startBtn = document.querySelector(".start");
+localStorage.setItem("btn", "focus");
+
+let initial, totalsecs, perc, paused, mins, seconds;
+
+startBtn.addEventListener("click", () => {
+    let btn = localStorage.getItem("btn");
+    
+    if(btn == "focus"){
+        mins = +localStorage.getItem("focusTime");
+    }else{
+        mins = +localStorage.getItem("breakTime");
+    }
+
+    seconds = mins * 60;
+    totalsecs = mins * 60;
+    setTimeout(decremenT(), 60);
+    startBtn.style.transform = "scale(0)";
+    paused = false;
+});
+
+function decremenT(){
+    mindiv.textContent = Math.floor(seconds / 60);
+    secdiv.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
+    if(circle.classList.contains("danger")){
+        circle.classList.remove("danger");
+    }
+
+    if(seconds > 0){
+        perc = Math.ceil(((totalsecs - seconds) / totalsecs) * 100);
+        setProgress(perc);
+        seconds--;
+        initial = window.setTimeout("decremenT()", 1000);
+        if(seconds < 10){
+            circle.classList.add("danger");
+        }
+    }else{
+        mins = 0;
+        seconds = 0;
+        bell.play();
+        let btn = localStorage.getItem("btn");
+
+        if(btn == "focus"){
+            startBtn.textContent = "start break";
+            startBtn.classList.add("break");
+            localStorage.setItem("btn", "break");
+        }else{
+            startBtn.classList.remove("break");
+            startBtn.textContent = "start focus";
+            localStorage.setItem("btn", "break");
+        }
+        startBtn.style.transform = "scale(1)";
+    }
+
+}
+
+
+//progress-------------------------------------
+const circle = document.querySelector(".progress-ring__circle");
+const radius = circle.r.baseVal.value;
+const circumference = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = circumference;
+circle.style.strokeDashoffset = circumference;
+
+function setProgress(percent){
+    const offset = circumference - (percent / 100) * circumference;
+    circle.style.strokeDashoffset = offset;
+}
+
+
+//settings----------------------------------
+const focusTimeInput = document.querySelector("#focusTime");
+const breakTimeInput = document.querySelector("#breakTime");
+const pauseBtn = document.querySelector(".pause");
+const focusPH = document.querySelector(".studyInput");
+const breakPH = document.querySelector(".breakInput");
+
+
+
+document.querySelector("form").addEventListener("submit", (e) =>{
+    e.preventDefault();
+    localStorage.setItem("focusTime", focusTimeInput.value);
+    localStorage.setItem("breakTime", breakTimeInput.value);
+    focusPH.value = localStorage.focusTime;
+    breakPH.value = localStorage.breakTime;
+});
+
+document.querySelector(".reset").addEventListener("click", () =>{
+    startBtn.style.transform = "scale(1)";
+    clearTimeout(initial);
+    setProgress(0);
+    mindiv.textContent = 0;
+    secdiv.textContent = 0;
+});
+
+pauseBtn.addEventListener("click", () => {
+    if(paused == undefined){
+        return;
+    }
+    if(paused){
+        paused = false;
+        initial = setTimeout("decremenT()", 60);
+        pauseBtn.textContent = "pause";
+        pauseBtn.classList.remove("resume");
+    }else{
+        clearTimeout(initial);
+        pauseBtn.textContent = "resume";
+        pauseBtn.classList.add("resume");
+        paused = true;
+    }
+});
+
+
+
+
+$(document).ready(function(){
+    var status = 0;
+    $("#hiddenForm").hide();
+    $("#buttonHide").click(function(e){
+        if(status == 0){
+            $("#hiddenForm").show(500);
+            status = 1;
+        }else{
+            $("#hiddenForm").hide(500);
+            status = 0;
+        }
+    });
+
+
+
+    focusPH.value = localStorage.focusTime;
+    breakPH.value = localStorage.breakTime;
+})
